cc406a534da3ec1321ca5d900db2d34f2947b718
Sr. Patinho <83667953+martimcabral@users.noreply.github.com>
2024-03-09 13:16:01 -0100

    Add files via upload

--- screens/news_list.dart
+++ screens/news_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../models/news.dart';
 
 class NewsListScreen extends StatefulWidget {
   const NewsListScreen({super.key});
@@ -8,6 +9,14 @@ class NewsListScreen extends StatefulWidget {
 }
 
 class _NewsListScreenState extends State<NewsListScreen> {
+  late List<News> _newsList;
+
+  @override
+  void initState() {
+    super.initState();
+    _newsList = [];
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
